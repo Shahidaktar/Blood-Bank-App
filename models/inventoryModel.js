4e0af3634e3c71119169c5c20b6bddb34e674f59
Shahidaktar <shahidaktar897@gmail.com>
2024-10-07 17:27:31 +0530

    Update

--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,39 +1,46 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const inventorySchema=new mongoose.Schema({
-    inventoryType:{
-        type:String,
-        required:[true,'inventory type required'],
-        enum:['in','out']
+const inventorySchema = new mongoose.Schema(
+  {
+    inventoryType: {
+      type: String,
+      required: [true, "inventory type required"],
+      enum: ["in", "out"],
     },
-    bloodGroup:{
-        type:String,
-        required:[true,'blood group is required'],
-        enum:['O+','O-','AB+','AB-','A+','A-','B+','B-']
+    bloodGroup: {
+      type: String,
+      required: [true, "blood group is required"],
+      enum: ["O+", "O-", "AB+", "AB-", "A+", "A-", "B+", "B-"],
     },
-    quantity:{
-        type:Number,
-        required:[true,'blood quantity is required']
+    quantity: {
+      type: Number,
+      required: [true, "blood quantity is required"],
     },
-    organization:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'users',
-        required:[true,'organization is required']
+    email: {
+      type: String,
+      required: [true, "Email is required"],
     },
-    hospital:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'users',
-        required: function(){
-            return this.inventoryType==='out';
-        }
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "organization is required"],
     },
-    donar:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'users',
-        required: function(){
-            return this.inventoryType==='in';
-        }
-    }
-},{timestamps:true});
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "out";
+      },
+    },
+    donar: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "in";
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports=mongoose.model('inventory',inventorySchema);
+module.exports = mongoose.model("inventory", inventorySchema);
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,10 +1,42 @@
-const express=require("express");
+const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { createInventoryController, getInventoryController } = require("../controllers/inventoryController");
-const router=express.Router();
+const {
+  createInventoryController,
+  getInventoryController,
+  getDonarsController,
+  getHospitalsController,
+  getOrganizationController,
+  getOrganizationForHospitalController,
+  getInventoryHospitalController,
+  getRecentInventoryController,
+} = require("../controllers/inventoryController");
+const { getOrganizationsListController } = require("../controllers/adminController");
+const router = express.Router();
 
-router.post('/create-inventory',authMiddleware,createInventoryController);
+router.post("/create-inventory", authMiddleware, createInventoryController);
 
-router.get('/get-inventory',authMiddleware,getInventoryController)
+router.get("/get-inventory", authMiddleware, getInventoryController);
 
-module.exports=router
+router.get("/get-recent-inventory", authMiddleware, getRecentInventoryController);
+
+router.post(
+  "/get-inventory-hospitals",
+  authMiddleware,
+  getInventoryHospitalController
+);
+
+router.get("/get-donars", authMiddleware, getDonarsController);
+
+router.get("/get-hospitals", authMiddleware, getHospitalsController);
+
+router.get("/get-organizations", authMiddleware, getOrganizationController);
+
+router.get("/get-all-organizations", authMiddleware, getOrganizationsListController);
+
+router.get(
+  "/get-organizations-for-hospital",
+  authMiddleware,
+  getOrganizationForHospitalController
+);
+
+module.exports = router;
